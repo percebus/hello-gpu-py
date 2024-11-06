7518ad828a026138f57eff2fb08304dd9553d169
JC Guerrero <JCystems@gMail.com>
2024-11-06 09:55:35 -0600

    fix(scripts/pip/install_torch.ba.sh): Added non-interactive flag

--- scripts/pip/install_torch.ba.sh
+++ scripts/pip/install_torch.ba.sh
@@ -1,4 +1,4 @@
 # SRC: https://pytorch.org/get-started/locally/#supported-windows-distributions
 
-pip3 uninstall torch torchvision torchaudio
+pip3 uninstall torch torchvision torchaudio --yes
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
